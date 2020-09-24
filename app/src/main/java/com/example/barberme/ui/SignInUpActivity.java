64e031af653eb6267728646919f84c7e669279c0
eliadgavri <eliad159@gmail.com>
2020-09-24 20:41:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/barberme/ui/SignInUpActivity.java
@@ -0,0 +1,37 @@
+package com.example.barberme.ui;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.barberme.R;
+
+public class SignInUpActivity extends AppCompatActivity {
+
+    final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signinup);
+        getSupportFragmentManager().beginTransaction().add(R.id.container, new SignInFragment(), TAG).commit();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, new SignUpFragment(), TAG).addToBackStack(null).commit();
+            }
+        });
+
+
+    }
+}
